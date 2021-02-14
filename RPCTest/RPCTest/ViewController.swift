15758474f6c2859524ebce5d337febaac6032468
Hanwe Lee <mobiledev@kakao.com>
2021-02-14 12:31:16 +0900

    init

---
+++ RPCTest/RPCTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RPCTest
+//
+//  Created by hanwe on 2021/02/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
